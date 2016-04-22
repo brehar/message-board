960e987fdcc0e0fc75c9dee128a2c62ccbfc8c08
Brett Hartman <brett.hartman@mac.com>
2016-04-21 22:21:12 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,89 @@
+'use strict';
+
+const PORT = process.env.PORT || 3000;
+
+var express = require('express');
+var morgan = require('morgan');
+var bodyParser = require('body-parser');
+
+var Message = require('./models/message');
+
+var app = express();
+
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.set('view engine', 'jade');
+
+app.route('/api/messages').get((req, res, next) => {
+    Message.findAll((err, messages) => {
+        if (err) {
+            return res.status(400).send(err);
+        }
+
+        res.send(messages);
+    });
+}).post((req, res, next) => {
+    Message.create(req.body, err => {
+        if (err) return res.status(400).send(err);
+
+        res.send();
+    });
+});
+
+app.route('/api/messages/:id').get((req, res, next) => {
+    var id = req.params.id;
+
+    Message.findById(id, (err, message) => {
+        if (err || !message) {
+            return res.status(400).send(err || 'Message not found.');
+        }
+
+        res.send(message);
+    });
+}).put((req, res, next) => {
+    var id = req.params.id;
+
+    Message.removeById(id, err => {
+        if (err) return res.status(400).send(err);
+    });
+
+    Message.create(req.body, err => {
+        if (err) return res.status(400).send(err);
+
+        res.send();
+    });
+}).delete((req, res, next) => {
+    var id = req.params.id;
+
+    Message.removeById(id, err => {
+        if (err) return res.status(400).send(err);
+
+        res.send();
+    });
+});
+
+app.get('/', (req, res, next) => {
+    res.render('index', {
+        indexRoute: true
+    });
+});
+
+app.get('/messages', (req, res, next) => {
+    Message.findAll((err, messages) => {
+        if (err) {
+            return res.status(400).send(err);
+        }
+
+        res.render('messages', {
+            messagesRoute: true,
+            messages: messages
+        });
+    });
+});
+
+app.listen(PORT, err => {
+    console.log(err || `Server listening on port ${PORT}`);
+});
---
+++ models/message.js
@@ -0,0 +1,79 @@
+'use strict';
+
+var fs = require('fs');
+var path = require('path');
+var moment = require('moment');
+
+var dataFile = path.join(__dirname, '../data/messages.json');
+
+exports.findAll = function(cb) {
+    fs.readFile(dataFile, (err, data) => {
+        if (err) {
+            cb(err);
+            return;
+        }
+
+        try {
+            var messages = JSON.parse(data);
+        } catch (err) {
+            return cb(err);
+        }
+
+        messages.reverse();
+
+        cb(null, messages);
+    })
+};
+
+exports.create = function(message, cb) {
+    if (!message.message || !message.name || !message.email) {
+        return cb('A message, your name, and your email are required.');
+    }
+
+    this.findAll((err, messages) => {
+        if (err) {
+            return cb(err);
+        }
+
+        var newMessage = {
+            message: message.message,
+            name: message.name,
+            email: message.email,
+            image: message.image,
+            timestamp: message.timestamp,
+            id: message.id
+        };
+
+        messages.push(newMessage);
+
+        fs.writeFile(dataFile, JSON.stringify(messages), err => {
+            cb(err);
+        });
+    });
+};
+
+exports.findById = function(id, cb) {
+    if (!id) return cb('Message id required.');
+
+    this.findAll((err, messages) => {
+        if (err) return cb(err);
+
+        var message = messages.filter(message => message.id === id)[0];
+
+        cb(null, message);
+    });
+};
+
+exports.removeById = function(id, cb) {
+    if (!id) return cb('Message id required.');
+    
+    this.findAll((err, messages) => {
+        if (err) return cb(err);
+        
+        messages = messages.filter(message => message.id !== id);
+
+        fs.writeFile(dataFile, JSON.stringify(messages), err => {
+            cb(err);
+        });
+    });
+};
